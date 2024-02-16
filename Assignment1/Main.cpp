12131cca496f8dacf999fb28e9574805f07f13a6
Michael Walker <micwalk2@uat.edu>
2024-02-16 10:57:08 -0800

    Minor formatting change

--- Assignment1/Main.cpp
+++ Assignment1/Main.cpp
@@ -13,18 +13,18 @@
 // 
 //  - Create a base class with a display method 
 //    for encapsulation plus at least 3 private 
-//    attributes and 2 constructors.
+//    attributes and 2 constructors
 // 
 //  - Create 2 classes that inherit from the base 
 //    class.  Each must have at least 3 additional 
 //    private attributes and 3 methods including 
 //    one that updates, one that displays (using 
 //    the base class display method as well) and 
-//    at least 1 constructor.
+//    at least 1 constructor
 // 
 //  - Create 2 methods in one of the derived 
 //    classes (not the constructor) with the 
-//    same name and different parameters.
+//    same name and different parameters
 //-----------------------------------------------
 
 #include <iostream>
