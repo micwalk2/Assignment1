16c660a923a3f3b073f7c6b2911c14dffdbaf582
Michael Walker <micwalk2@uat.edu>
2024-02-16 10:51:29 -0800

    Commented in header and TODO, and roughed out game loop in main() function.

---
+++ Assignment1/Main.cpp
@@ -0,0 +1,43 @@
+// Michael Walker
+// CSC275: C++ Programming II
+//
+// Week 1 Assignment: Object Oriented Programming
+
+//-----------------------------------------------
+// TODO:
+//  - Cool intro to game
+// 
+//  - Add a game loop into the main() function
+// 
+//  - Create a class called GameStructure
+// 
+//  - Create a base class with a display method 
+//    for encapsulation plus at least 3 private 
+//    attributes and 2 constructors.
+// 
+//  - Create 2 classes that inherit from the base 
+//    class.  Each must have at least 3 additional 
+//    private attributes and 3 methods including 
+//    one that updates, one that displays (using 
+//    the base class display method as well) and 
+//    at least 1 constructor.
+// 
+//  - Create 2 methods in one of the derived 
+//    classes (not the constructor) with the 
+//    same name and different parameters.
+//-----------------------------------------------
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	bool isRunning = true;
+
+	while (isRunning)
+	{
+		// TODO: Implement game loop functionality
+	}
+
+	return 0;
+}
